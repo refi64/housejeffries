d7e7cbd318f934cc3747595e6d8e4c9f5fb92804
Ian Grant Jeffries <ian@housejeffries.com>
2014-08-13 23:41:31 +0200

    Make a rough draft of the site.

--- site.hs
+++ site.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import Data.List (isInfixOf)
+import System.FilePath.Posix  (takeBaseName,takeDirectory,(</>),splitFileName)
 
 
 --------------------------------------------------------------------------------
@@ -15,53 +17,61 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
     match "templates/*" $ compile templateCompiler
 
+    match "favicon.ico" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "index.md" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/base.html" defaultContext
+            >>= relativizeUrls
+
+    match "articles/*" $ do
+        route $ niceRoute
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/page.html" postCtx
+            >>= loadAndApplyTemplate "templates/base.html" postCtx
+            >>= relativizeUrls
+            >>= removeIndexHtml
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+--------------------------------------------------------------------------------
+-- Create "foo/index.html" pages instead of "foo.html" pages. These show up as
+-- just "foo" in the browser, which looks nicer than "foo.html".
+
+-- Code from here:
+-- https://raw.githubusercontent.com/Abizern/hblog/master/hblog.hs
+--
+-- Though he probably got the idea from here:
+-- http://yannesposito.com/Scratch/en/blog/Hakyll-setup/
+
+niceRoute :: Routes
+niceRoute = customRoute createIndexRoute
+  where
+    createIndexRoute ident =
+      takeDirectory p </> takeBaseName p </> "index.html"
+      where
+        p = toFilePath ident
+
+-- Replace an url of the form foo/bar/index.html by foo/bar
+removeIndexHtml :: Item String -> Compiler (Item String)
+removeIndexHtml item = return $ fmap (withUrls removeIndexStr) item
+
+-- Removes the .html component of a URL if it is local
+removeIndexStr :: String -> String
+removeIndexStr url = case splitFileName url of
+  (dir, "index.html") | isLocal dir -> dir
+                      | otherwise   -> url
+  _                                 -> url
+  where
+    isLocal uri = not ("://" `isInfixOf` uri)
+
+--------------------------------------------------------------------------------

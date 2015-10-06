2336f2ba33c8dd27d23d4dc1f01753a4b4ff9bdd
Ian Grant Jeffries <ian@housejeffries.com>
2015-10-06 19:52:05 -0400

    First attempt at fixing urls.

--- Site.hs
+++ Site.hs
@@ -1,6 +1,43 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+-- * The biggest thing going down here is a URL hack.
+--
+-- I don't want index.html to show up in URLs.
+--
+-- I want pages to have nice URLs without trailing slashes.
+-- URLs with a slash should redirect to a URL without one:
+--
+--   http://housejeffries.com/page/4
+--
+-- I want page.md files to have relative links to resources within them:
+--
+--   ./some-image.jpg
+--
+-- I want both of these things to work at the same time.
+--
+-- In order to get the first I have rewrite rules in resources/.htaccess.
+-- Note that that this takes place on the server, so just because things
+-- are working fine locally doesn't mean they will be after pushing.
+--
+-- This however breaks relative links to other resources. A link such as
+-- ./image.jpg (that resolves to /pages/2/image.jpg) becomes a link
+-- that resolves to /pages/image.jpg. My first way to fix this was just
+-- to write the link to ./2/image.jpg, but that was sad.
+--
+-- Currently I use the <base> URL element, but only in non-homepage pages.
+--
+-- Note that <base> affects both anchor links within documents
+-- as well as links to other documents, but it seems to be working
+-- at the moment. I think I'm in good shape since I'm just setting
+-- the resolving path to where the resource actually lives, not using
+-- <base> to lie about where it's located.
+--
+-- Also note that Hakyll does some rewriting too, so /css/minimal.css
+-- in the template becomes ../../css/minimal.css when viewed in an
+-- output file such as page/2/index.html. TODO: find out why it does
+-- this.
+
 module Main where
 
 import           Control.Monad
@@ -42,10 +79,9 @@ main = hakyllWith myConfig $ do
   match "_pages/1/page.md" $ do
     route $ constRoute "index.html"
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/base.html" (defaultContext <> pageIdContext)
+      >>= loadAndApplyTemplate "templates/base.html" homepageContext
       >>= relativizeUrls
 
-  -- Output pages as HTML.
   match "_pages/*/page.md" $ do
     route $ customRoute (\x -> let pageId = unsafePageId . splitDirectories . toFilePath $ x
                                in "page" </> pageId </> "index.html")
@@ -72,10 +108,18 @@ main = hakyllWith myConfig $ do
 
       renderAtom myFeedConfig feedCtx pages
 
+homepageContext :: Context String
+homepageContext = pageIdContext
+               <> boolField "isHomepage" (const True)
+               <> defaultContext -- Goes last so that it will be overwritten in case of conflict.
+
 pageContext :: Context String
 pageContext = dateField "published" "%B %e, %Y" -- e.g. February 3, 2015
            <> pageIdContext
            <> defaultContext
+
+pageIdContext :: Context a
+pageIdContext = field "pageId" $ return . unsafePageId . splitDirectories . toFilePath . itemIdentifier
 
 unsafePageId :: [FilePath] -> String
 unsafePageId xs =
@@ -84,15 +128,11 @@ unsafePageId xs =
     Nothing -> error $ "Page ID isn't an integer: " <> pageId
     Just _  -> pageId
 
-pageIdContext :: Context a
-pageIdContext = field "pageId" $ return . unsafePageId . splitDirectories . toFilePath . itemIdentifier
-
 --------------------------------------------------
 -- * Index trick
 --------------------------------------------------
 
--- | Create "foo/index.html" pages instead of "foo.html" pages. These show up as
--- just "foo" in the browser, which looks nicer than "foo.html".
+-- | Create "foo/index.html" pages instead of "foo.html" pages.
 --
 -- Code from here:
 -- https://raw.githubusercontent.com/Abizern/hblog/master/hblog.hs
